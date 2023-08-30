abb20d1494b48427d6791eb98d1df5bcaa1f0ed3
Artur Karczmarczyk <artur@ideaspot.pl>
2023-08-30 09:17:36 +0200

    form validation

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -18,6 +18,11 @@ export default function Checkout({cart, emptyCart}) {
     const [address, setAddress] = useState(emptyAddress);
     const [status, setStatus] = useState(STATUS.IDLE);
     const [saveError, setSaveError] = useState(null);
+    const [touched, setTouched] = useState({});
+
+    // Derived state
+    const errors = getErrors(address);
+    const isValid = Object.keys(errors).length === 0;
 
     function handleChange(e) {
         e.persist(); // persist the event so it's available in setAddress
@@ -30,19 +35,33 @@ export default function Checkout({cart, emptyCart}) {
     }
 
     function handleBlur(event) {
-        // TODO
+        event.persist();
+        setTouched((cur) => {
+            return {...cur, [event.target.id]: true};
+        });
     }
 
     async function handleSubmit(event) {
         event.preventDefault();
         setStatus(STATUS.SUBMITTING);
-        try {
-            await saveShippingAddress(address);
-            emptyCart();
-            setStatus(STATUS.COMPLETED);
-        } catch (e) {
-            setSaveError(e);
+        if (isValid) {
+            try {
+                await saveShippingAddress(address);
+                emptyCart();
+                setStatus(STATUS.COMPLETED);
+            } catch (e) {
+                setSaveError(e);
+            }
+        } else {
+            setStatus(STATUS.SUBMITTED);
         }
+    }
+
+    function getErrors(address) {
+        const result = {};
+        if (!address.city) result.city = "City is required";
+        if (!address.country) result.country = "Country is required";
+        return result;
     }
 
     if (saveError) throw saveError;
@@ -53,6 +72,16 @@ export default function Checkout({cart, emptyCart}) {
     return (
         <>
             <h1>Shipping Info</h1>
+            {!isValid && status === STATUS.SUBMITTED && (
+                <div role="alert">
+                    <p>Please fix the following errors:</p>
+                    <ul>
+                        {Object.keys(errors).map((key) => {
+                            return <li key={key}>{errors[key]}</li>;
+                        })}
+                    </ul>
+                </div>
+            )}
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="city">City</label>
@@ -64,6 +93,9 @@ export default function Checkout({cart, emptyCart}) {
                         onBlur={handleBlur}
                         onChange={handleChange}
                     />
+                    <p role="alert">
+                        {(touched.city || status === STATUS.SUBMITTED) && errors.city}
+                    </p>
                 </div>
 
                 <div>
@@ -81,6 +113,9 @@ export default function Checkout({cart, emptyCart}) {
                         <option value="United Kingdom">United Kingdom</option>
                         <option value="USA">USA</option>
                     </select>
+                    <p role="alert">
+                        {(touched.country || status === STATUS.SUBMITTED) && errors.country}
+                    </p>
                 </div>
 
                 <div>
